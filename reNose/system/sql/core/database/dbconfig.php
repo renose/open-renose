dcb0c05e339b29da2b5e61abda839d357b9f4b4b
Patrick Hafner <djpadi@gmail.com>
2011-02-03 13:45:22 +0000

    Falsch kopierte Dateien geloescht
    git-svn-id: https://open-renose.googlecode.com/svn/trunk@15 f4d3dde6-3f83-6e2d-8f47-08daef469822

--- reNose/system/sql/core/database/dbconfig.php
+++
@@ -1,8 +0,0 @@
-<?PHP
-	//$dbpraefix = "orn_"; //Für die Installation auf anderen Server (in Zukunft) => keine eigene Datenbank für reNose >Konflikte<
-	$dbpraefix = "";
-	$dbhost = "localhost";
-	$dbuser = "renose";
-	$dbpassword = "open";
-	$db = "renose";
-?>
--- reNose/system/sql/core/database/dbconnect.php
+++
@@ -1,6 +0,0 @@
-<?PHP
-	include('system\core\database\dbconfig.php');
-	
-	$connection = mysql_connect($dbhost, $dbuser, $dbpassword);
-	mysql_select_db($db, $connection);
-?>
