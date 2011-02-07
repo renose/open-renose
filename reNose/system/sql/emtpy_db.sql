2aee4e8b529e4ba309f3be7466e1133271ce876f
Simon Wörner <Simon.Woerner@gmail.com>
2011-02-07 19:39:44 +0000

    DB Update Batch
    git-svn-id: https://open-renose.googlecode.com/svn/trunk@25 f4d3dde6-3f83-6e2d-8f47-08daef469822

---
+++ reNose/system/sql/emtpy_db.sql
@@ -0,0 +1,7 @@
+DROP DATABASE renose;
+
+CREATE DATABASE renose;
+
+
+
+DROP USER renose@localhost;
---
+++ reNose/system/sql/update_db.bat
@@ -0,0 +1,4 @@
+C:\xampp\mysql\bin\mysql --user=root < emtpy_database.sql
+C:\xampp\mysql\bin\mysql --user=root < user.sql
+C:\xampp\mysql\bin\mysql --user=renose --password=open renose < renose.sql;
+PAUSE
