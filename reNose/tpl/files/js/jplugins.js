aca8080685c431279de1fc3d9b2a1d78bd30c438
Simon Wörner <simon.woerner@gmail.com>
2012-02-26 02:13:39 +0100

    js
        git-svn-id: https://open-renose.googlecode.com/svn/trunk@54 f4d3dde6-3f83-6e2d-8f47-08daef469822

--- reNose/tpl/files/js/jplugins.js
+++ reNose/tpl/files/js/jplugins.js
@@ -1,23 +1,84 @@
-/* $(document)
-	.ready(function() {
-		$("li#test").ready(function(){
-			$('#seiten').html('<p><img src="files/images/ajax-loader.gif" /></p>');
-			var param = "Welt";
-			$.ajax({
-		    url : "http://google.de",
-		    success: function(data) {
-			$("#seiten").html(data);
-	    	},
-		    statusCode: {404: function() {
-    			$("#seiten").text('Seite nicht gefunden');
-			}}
-		});
-	});
-}); */
+$(document).ready(function(){
 
-$(function(){$('#menubox').tabs();});
+    jQuery("div#navigation ul").ready(function(){
+		$("#content").append('<p><img src="tpl/files/images/ajax-loader.gif" style="position:absolute;right:5px;top:5px;" /></p>');
+		var path = jQuery("#path").attr("href");
+		var params=document.URL.replace(path, "").replace(".html", "");
+		if (params == "") {params = "home"};
+	
+		$.ajax({
+		    type: "GET",
+	    	url : "functions/getPage.php",
+		    data: "id=" + params,
+			success: function(data) {
+			    $("#content").html(data);
+			}
+	    });
+    });
+
+
+    $("div#navigation ul li a").click(function(){
+		//var param = ($(this).attr('id').replace("pageid_", ""));
+		$("#content").append('<p><img src="tpl/files/images/ajax-loader.gif" style="position:absolute;right:5px;top:5px;" /></p>');
+	
+		/*$.ajax({
+	    	type: "GET",
+		    url : "functions/getPage.php",
+		    data: "id="+param,
+	    	success: function(data) {
+				$("#content").html(data);
+		    },
+		    statusCode: {404: function() {
+				$("#content").text('Seite nicht gefunden');
+			}}
+	    });*/
+    });
+    
+});
 
 function returnToLastPage() {
-  /*document.location.href= "http://www.java2s.com"*/
-  history.back();
+    /*document.location.href= "http://www.java2s.com"*/
+    history.back();
 }
+
+
+jQuery.fn.alertbox = function(options) {
+    var defaults = {
+    };
+    var options = jQuery.extend(defaults, options);
+
+    return this.each(function() {
+	var obj = jQuery(this);
+	var objheight = obj.height();
+	var objwidth = obj.width();
+	var screenwidth = (window.innerWidth / 2) - (objwidth / 2);
+	var screenheight = (window.innerHeight / 2) - (objheight / 2);
+
+	obj.ready(function(){
+	    obj.css({
+		"display" : "block",
+		"top" : "" + screenheight + "px",
+		"left" : "" + screenwidth + "px",
+		"visibility" : "visible"
+	    })
+	    ;
+	});
+
+	jQuery("input#closealertbox").click(function(){
+	    jQuery("div.alertbox").fadeOut();
+	    jQuery("div.overlay").fadeOut();
+	    return false;
+	});
+	jQuery("div.overlay").click(function(){
+	    jQuery("div.alertbox").fadeOut();
+	    jQuery("div.overlay").fadeOut();
+	    return false;
+	});
+    });
+}
+
+
+$(function(){
+    $('.alertbox').alertbox();
+});
+
