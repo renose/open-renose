8fc01d4ace0963f8e50241431b2baf4a9698c5ec
root <root@Sleipnir.(none)>
2012-05-04 14:04:55 +0200

    database settings updated

--- app/Config/database.php
+++ app/Config/database.php
@@ -65,7 +65,7 @@ class DATABASE_CONFIG {
 		'host' => 'localhost',
 		'login' => 'renose',
 		'password' => 'open',
-		'database' => 'renose_dev',
+		'database' => 'renose',
 		'prefix' => '',
 		'encoding' => 'utf8'
 	);
