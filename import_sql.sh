45aaa4a24bef766107bdad979b9fe1c2933fc556
Simon Wörner <simon.woerner@gmail.com>
2012-07-03 15:56:10 +0200

    added new update_database scripts

--- import_sql.sh
+++
@@ -1,3 +0,0 @@
-mysql --user=renose --password=dev -e "DROP DATABASE renose"
-mysql --user=renose --password=dev -e "CREATE DATABASE renose"
-mysql --user=renose --password=dev --database=renose --default-character-set=utf8 < renose_dev.sql
---
+++ update_database.bat
@@ -0,0 +1,2 @@
+cd app
+Console\cake schema update
